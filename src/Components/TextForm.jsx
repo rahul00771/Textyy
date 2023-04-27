925aa9fd2a29ca3e8aa34eb62bdff2da5e6fceb7
Rahul Sah <rahulmsd0777@gmail.com>
2023-04-28 01:31:36 +0530

    Added reset, copy and repeat functionality

--- src/Components/TextForm.jsx
+++ src/Components/TextForm.jsx
@@ -9,6 +9,11 @@ export default function  TextForm()
     const [text, setText] = useState("");
     let newText = "";
 
+    const handleTextChange = (event)=>
+    {
+        setText(event.target.value);
+    }
+
     function handleUpClick()
     {
         newText = text.toUpperCase();
@@ -21,30 +26,54 @@ export default function  TextForm()
         setText(newText);
     }
 
-    const handleChange = (event)=>
-    {
-        setText(event.target.value);
+    const handleClearClick = ()=>{
+        newText = "";
+        setText(newText);
+        document.querySelector(".xInput").value = 0;
     }
 
+    let x = 1;
+    const handleRepeatChange = (event)=>{
+        x = event.target.value;
+    }
+
+    const handleRepeatClick = ()=>{
+        newText = text.repeat(x);
+        setText(newText);
+    }
+    
     function wordCount(str) {
         var m = str.match(/[^\s]+/g)
         return m ? m.length : 0;
       }
 
+    const copyToClipboard = ()=>{
+
+        navigator.clipboard.writeText(text);
+    }
+
+    const myStyle = {
+        color : 'red',
+        backgroundColor : 'black'
+    }
+
     return(
         <>
-        <div className='container'>
-            <h2>Enter the text to analyze</h2>
+        <div className='container' >
+            <h1>Enter the text to analyze</h1>
             <div className='textForm'>
-                <textarea onChange={handleChange} rows={24} type='text' placeholder='Enter text here' value={text} />
+                <textarea onChange={handleTextChange} rows={24} type='text' placeholder='Enter text here' value={text} />
             </div>
             <div className='btnContainer'>
                 <button onClick={handleUpClick}>Convert to UpperCase</button>
                 <button onClick={handleLoClick}>Convert to LowerCase</button>
+                <button onClick={handleClearClick}>Reset</button>
             </div>
+            <button onClick={handleRepeatClick}>Repeat 'x' number of times</button>
+            <input className='xInput' size={1} onChange={handleRepeatChange} placeholder ='   x'></input>
             <h2>Preview:</h2>
-            <p>The text has {wordCount(text)} words and {text.length} characters.</p>
-            <p>{text}</p>
+            <p>The text has {wordCount(text)} words and {text.length} characters<img onClick={copyToClipboard} className='copyImage' src='../public/copyIcon.png'></img></p>
+            <p className='output'><b>{text}</b></p>
         </div>
         </>
     );
