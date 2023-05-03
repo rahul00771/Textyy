6c696c5b32e7ca8afb9d450b9c746ddc88c2fb97
Rahul Sah <rahulmsd0777@gmail.com>
2023-05-03 22:50:02 +0530

    Final

--- src/Components/TextForm.jsx
+++ src/Components/TextForm.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
-import TextForm from './Components/TextForm';
+import React, { useState } from 'react'
 
 //This component is the componenet with the text form
 //In this comp, state is used to analyze it and perform the necessary method
 
 
-export default function  TextForm()
+export default function  TextForm(props)
 {
     const [text, setText] = useState("");
     let newText = "";
@@ -54,17 +53,22 @@ export default function  TextForm()
         navigator.clipboard.writeText(text);
     }
 
+    const myStyle = {
+        color : 'red',
+        backgroundColor : 'black'
+    }
+
     return(
         <>
         <div className='container' >
             <h1>Enter the text to analyze</h1>
             <div className='textForm'>
-                <textarea onChange={handleTextChange} rows={24} type='text' placeholder='Enter text here' value={text} />
+            <textarea style={props.textFormColor} onChange={handleTextChange} rows={24} type='text' placeholder='Enter text here' value={text} />
             </div>
             <div className='btnContainer'>
-                <button onClick={handleUpClick}>Convert to UpperCase</button>
-                <button onClick={handleLoClick}>Convert to LowerCase</button>
-                <button onClick={handleClearClick}>Reset</button>
+            <button style={props.textFormColor} onClick={handleUpClick}>Convert to UpperCase</button>
+                <button style={props.textFormColor} onClick={handleLoClick}>Convert to LowerCase</button>
+                <button style={props.textFormColor} onClick={handleClearClick}>Reset</button>
             </div>
             <button style={props.textFormColor} onClick={handleRepeatClick}>Repeat 'x' number of times</button>
             <input style={props.textFormColor} className='xInput' size={1} onChange={handleRepeatChange} placeholder ='   x'></input>
